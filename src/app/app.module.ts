10a3a991975c02bef43c72a48a7d1146a42d3a3c
Pascal Briffard <pascal.briffard@gmail.com>
2021-09-03 14:39:11 +0200

    first depot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
-import { NgModule } from '@angular/core';
+import {  NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MonPremierComponent } from './mon-premier/mon-premier.component';
+import { AppareilComponent } from './appareil/appareil.component';
+import { FormsModule } from '@angular/forms';
 
+// Importer les variables locales en Français
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeFR from '@angular/common/locales/fr';
+
+// Importer mes services
+import { AppareilService } from './services/appareil.service';
+
+registerLocaleData(localeFR)
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonPremierComponent,
+    AppareilComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'fr' },
+      AppareilService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
